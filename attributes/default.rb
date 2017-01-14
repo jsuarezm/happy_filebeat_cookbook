2df741e6394760ffb58aa8e542245ddabfa1590c
jsuarez <jesus.a.suarez@gmail.com>
2017-01-14 14:34:30 -0400

    Add vhost ip_deny

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,7 @@ default['filebeat']['elastic']['url'] = "url"
 default['filebeat']['elastic']['user'] = "user"
 default['filebeat']['elastic']['pass'] = "pass"
 
+default['vhost']['cert'] = ""
+default['vhost']['key'] = ""
+default['vhost']['cacert'] = ""
+
---
+++ recipes/ip_deny.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: happy_filebeat_cookbook
+# Recipe:: ip_deny
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template '/etc/httpd/sites-available/ip_deny.conf' do
+  source 'ip_deny.conf.erb'
+  mode '0440'
+  owner 'root'
+  group 'root'
+  variables({
+    cert_path: node['vhost']['cert'],
+    key_path: node['vhost']['key'],
+    cacert_path: node['vhost']['cacert']
+  })
+end
+
+link '/etc/httpd/sites-enabled/ip_deny.conf' do
+  to '/etc/httpd/sites-available/ip_deny.conf'
+  only_if 'test -f /etc/httpd/sites-available/ip_deny.conf'
+  not_if 'test -F /etc/httpd/sites-enabled/ip_deny.conf'
+end
+
+service 'httpd' do
+  action :restart
+end
+
