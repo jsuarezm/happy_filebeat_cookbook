7c748e464eb2a36e1c3c9ad4419437a12fd594a3
jsuarez <jesus.a.suarez@gmail.com>
2017-04-06 15:59:55 -0400

    Switch on/off TLS IP denied using custom json

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@ default['filebeat']['elastic']['url'] = "url"
 default['filebeat']['elastic']['user'] = "user"
 default['filebeat']['elastic']['pass'] = "pass"
 
+default['vhost']['tls'] = false
 default['vhost']['cert'] = ""
 default['vhost']['key'] = ""
 default['vhost']['cacert'] = ""
